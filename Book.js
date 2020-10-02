a4dbff913d39dd41138fe751820336248d9dc204
Mahek710 <710mahek@gmail.com>
2020-10-03 02:01:46 +0530

    yayy

---
+++ Book.js
@@ -0,0 +1,10 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
+  var slideIndex = 0;
+  showSlides();
