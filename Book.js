8017063a0c2e4d27fdeeb2379eae245ec0055fb4
Mahek710 <710mahek@gmail.com>
2020-10-03 03:17:44 +0530

    im sleeping now

--- Book.js
+++ Book.js
@@ -1,10 +1,8 @@
 function classToggle() {
-    const navs = document.querySelectorAll('.Navbar__Items')
-    
-    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
-  }
+  const navs = document.querySelectorAll('.Navbar__Items')
   
-  document.querySelector('.Navbar__Link-toggle')
-    .addEventListener('click', classToggle);
-  var slideIndex = 0;
-  showSlides();
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
