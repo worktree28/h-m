c18ead8f1263f3036cb8cf9d9d53a720a74b0ce7
worktree28 <hmishra2001@live.in>
2020-10-04 05:08:42 +0530

    nav trans plus slideshow fix

--- js/index.js
+++ js/index.js
@@ -29,9 +29,11 @@ document.querySelector('.Navbar__Link-toggle')
   window.onscroll = function () { 
       "use strict";
       if(document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
-          myNav.classList.add("nav-colored");
-      } 
-      else {
-          myNav.classList.add("nav-colored");
-      }
+        myNav.classList.add("nav-colored");
+        myNav.classList.remove("nav-transparent");
+    } 
+    else {
+        myNav.classList.add("nav-transparent");
+        myNav.classList.remove("nav-colored");
   };
+}
